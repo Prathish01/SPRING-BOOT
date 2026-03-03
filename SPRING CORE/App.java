c5fe63c1bc38fd9c53ef5ab15a31e59c3ef47f7e
Prathish S <prathish472@gmail.com>
2026-03-03 22:56:42 +0530

    Create App.java

---
+++ SPRING CORE/App.java
@@ -0,0 +1,15 @@
+package spring_demo.springid;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.hibernate.spring_beans.Car;
+
+public class App {
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        ApplicationContext context=new ClassPathXmlApplicationContext();
+    	Car bean= context.getBean(Car.class);
+    	System.out.println("car1");
+    }
+}
