e804133dcc18f497e41ff8bc9c0c6974a65ff19e
Prathish S <prathish472@gmail.com>
2026-04-25 20:47:31 +0530

    Update App.java

--- SPRING CORE/App.java
+++ SPRING CORE/App.java
@@ -5,6 +5,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.hibernate.spring_beans.Car;
 
+
+
 public class App {
     public static void main(String[] args) {
         //System.out.println("Hello World!");
